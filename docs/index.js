5d38a522bd1966f0c17062c5f4e43de731e97403
Damian <damiansuaz@gmail.com>
2025-04-08 09:51:30 -0400

    Attempt to order dates

--- docs/index.js
+++ docs/index.js
@@ -6,36 +6,43 @@ const logList = [
     log: 'Announce projects'
   },
   {
-    date: '9-3-2024',
+    date: '8-28-2024',
     log: 'Establish learning streaks'
   },
   {
-    date: '9-3-2024',
+    date: '8-1-2024',
     log: 'Roadmap'
   },
   {
-    date: '9-3-2024',
+    date: '8-22-2024',
     log: 'Submit solution, await feedback'
   },
   {
-    date: '9-3-2024',
+    date: '8-10-2024',
     log: 'Backend Ideas'
   },
   {
-    date: '9-3-2024',
+    date: '8-15-2024',
     log: 'Redis roadmap'
   },
   {
-    date: '9-3-2024',
+    date: '8-25-2024',
     log: 'Changelog page for the consumer'
   }
 ]
-
+function getOrderedDateList() {
+  logList.sort((a, b) => new Date(a.date) - new Date(b.date));
+  let output = [];
+  logList.forEach((obj) => {
+    
+  });
+  return output;
+}
 window.onload = function() {
   if(logSection && logList.length > 0) {
-    logList.forEach((l) => {
-      console.log(l.date);
-      console.log(l.log);
+    let dateOrder = logList.sort((a, b) => new Date(a.date) - new Date(b.date));
+    dateOrder.forEach((l) => {
+      console.log(l);
     });
-  } 
+  }
 };
